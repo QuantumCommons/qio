4ab37cbb52043282a7ada8b867420dbe1b355ba1
Valentin Macheret <vmacheret@scaleway.com>
2025-11-10 19:18:24 +0100

    feat(test): wip test

--- tests/test_cirq.py
+++ tests/test_cirq.py
@@ -11,11 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import numpy as np
-
-import cirq
-
-from cirq.circuits import Circuit
 from qsimcirq import QSimSimulator, QSimOptions
 
 from qio.core import (
@@ -29,57 +24,12 @@ from qio.core import (
 
 from qio.utils import CompressionFormat
 
-
-def _random_cirq_circuit(size: int) -> Circuit:
-    num_qubits = size
-    num_gate = size
-
-    # Create the qubits
-    qubits = [cirq.LineQubit(i) for i in range(num_qubits)]
-
-    # Initialize the circuit
-    circuit = Circuit()
-
-    for _ in range(num_gate):
-        random_gate = np.random.choice(["unitary", "cx", "cy", "cz"])
-
-        if random_gate in ["cx", "cy", "cz"]:
-            control_qubit = np.random.randint(0, num_qubits)
-            target_qubit = np.random.randint(0, num_qubits)
-
-            while target_qubit == control_qubit:
-                target_qubit = np.random.randint(0, num_qubits)
-
-            if random_gate == "cx":
-                circuit.append(cirq.CNOT(qubits[control_qubit], qubits[target_qubit]))
-            elif random_gate == "cy":
-                circuit.append(
-                    cirq.Y.controlled(1)(qubits[control_qubit], qubits[target_qubit])
-                )
-            elif random_gate == "cz":
-                circuit.append(cirq.CZ(qubits[control_qubit], qubits[target_qubit]))
-        else:
-            for q in range(num_qubits):
-                random_single_qubit_gate = np.random.choice(["H", "X", "Y", "Z"])
-                if random_single_qubit_gate == "H":
-                    circuit.append(cirq.H(qubits[q]))
-                elif random_single_qubit_gate == "X":
-                    circuit.append(cirq.X(qubits[q]))
-                elif random_single_qubit_gate == "Y":
-                    circuit.append(cirq.Y(qubits[q]))
-                elif random_single_qubit_gate == "Z":
-                    circuit.append(cirq.Z(qubits[q]))
-
-    # Add measurement
-    circuit.append(cirq.measure(*qubits, key="result"))
-
-    return circuit
-
+from qio.utils.circuit import random_square_cirq_circuit
 
 def test_global_cirq_flow():
     ### Client side
 
-    qc = _random_cirq_circuit(10)
+    qc = random_square_cirq_circuit(10)
     shots = 10
 
     program = QuantumProgram.from_cirq_circuit(qc)
